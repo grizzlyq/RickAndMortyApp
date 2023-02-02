2123286ba78c47ff7655f6c881a78adfe651deba
grizzlyq <pechenika73@yandex.ru>
2023-02-02 14:51:30 +0300

    Initial Commit

---
+++ Rick_And_Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick_And_Morty
+//
+//  Created by Дмитрий on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
